5ee820901566428395ed7e9a60ee91952fda8d5a
Jamil Frizzar <jfrizzar@protonmail.com>
2026-03-20 15:15:17 -0400

    Life Insurance subclass created. Next step is the DDC

---
+++ src/LifeInsurance.java
@@ -0,0 +1,37 @@
+public class LifeInsurance extends Insurance{
+    private String beneficiary;
+    private double faceValue;
+
+    //Constructor for AutoInsurance class
+    public LifeInsurance(String policyNumber, String policyOwner, double yearlyPremium, String beneficiary, double faceValue){
+        super(policyNumber, policyOwner, yearlyPremium);
+        setBeneficiary(beneficiary);
+        setFaceValue(faceValue);
+    }
+
+    //Get methods
+    public String getBeneficiary(){
+        return this.beneficiary;
+    }
+
+    public double getFaceValue(){
+        return this.faceValue;
+    }
+
+    //Set methods
+    public void setBeneficiary(String beneficiary){
+        this.beneficiary = beneficiary;
+    }
+
+    public void setFaceValue(double faceValue){
+        this.faceValue = faceValue;
+    }
+
+    //Special purpose methods
+    public String toString(){
+        return super.toString() +
+        "The beneficiary is: " + this.getBeneficiary() + "\n" +
+        "The face value is: " + this.getFaceValue() + "\n";
+    }
+
+}
